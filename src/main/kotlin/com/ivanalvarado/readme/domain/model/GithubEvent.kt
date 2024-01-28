202b9c7281916500853b03ac0a45b94c08f2983c
Ivan Alvarado <ivalvara21@gmail.com>
2024-01-27 18:11:40 -0800

    Add domain models for supported events

---
+++ src/main/kotlin/com/ivanalvarado/readme/domain/model/GithubEvent.kt
@@ -0,0 +1,46 @@
+package com.ivanalvarado.readme.domain.model
+
+sealed class GithubEvent {
+    abstract val repoUrl: String
+    abstract val createdAt: String
+
+    data class IssuesEvent(
+        val action: String,
+        val issueNumber: Int,
+        val title: String,
+        val issueUrl: String,
+        override val repoUrl: String,
+        override val createdAt: String
+    ): GithubEvent()
+
+    data class IssueCommentEvent(
+        val issueNumber: Int,
+        val issueCommentUrl: String,
+        override val repoUrl: String,
+        override val createdAt: String
+    ): GithubEvent()
+
+    data class PullRequestEvent(
+        val action: String,
+        val title: String,
+        val merged: Boolean,
+        val number: Int,
+        val prUrl: String,
+        override val repoUrl: String,
+        override val createdAt: String
+    ): GithubEvent()
+
+    data class CreateEvent(
+        val refType: String,
+        val ref: String,
+        override val repoUrl: String,
+        override val createdAt: String
+    ): GithubEvent()
+
+    data class DeleteEvent(
+        val refType: String,
+        val ref: String,
+        override val repoUrl: String,
+        override val createdAt: String
+    ): GithubEvent()
+}
