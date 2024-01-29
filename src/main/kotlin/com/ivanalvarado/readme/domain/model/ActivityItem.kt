ad45af310e63537ac8b1e355747bcd4cdb1acd0b
Ivan Alvarado <ivalvara21@gmail.com>
2024-01-28 20:22:47 -0800

    Override toString for ActivityItem

--- src/main/kotlin/com/ivanalvarado/readme/domain/model/ActivityItem.kt
+++ src/main/kotlin/com/ivanalvarado/readme/domain/model/ActivityItem.kt
@@ -3,4 +3,8 @@ package com.ivanalvarado.readme.domain.model
 data class ActivityItem(
     val message: String,
     val date: String
-)
+) {
+    override fun toString(): String {
+        return "**$date** - $message"
+    }
+}
