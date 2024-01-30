bff827af8cc95c91fd017ac7c1ef482fdc440c56
Ivan Alvarado <ivalvara21@gmail.com>
2024-01-29 17:10:16 -0800

    Increase results per_page to 100

--- src/main/kotlin/com/ivanalvarado/readme/network/GithubService.kt
+++ src/main/kotlin/com/ivanalvarado/readme/network/GithubService.kt
@@ -4,8 +4,12 @@ import com.ivanalvarado.readme.network.model.EventsJson
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface GithubService {
     @GET("/users/{login}/events/public")
-    suspend fun userEvents(@Path("login") login: String): Response<List<EventsJson>>
+    suspend fun userEvents(
+        @Path("login") login: String,
+        @Query("per_page") perPage: Int = 100
+    ): Response<List<EventsJson>>
 }
